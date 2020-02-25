23c570e33b27f90a27bba158935e5832a3ec4232
Razvan <tangent.jotey@gmail.com>
2020-02-25 10:23:37 +0100

    Update hello.php

--- examples/hello.php
+++ examples/hello.php
@@ -4,6 +4,10 @@ use com\github\tncrazvan\jphp\Jphp;
 use com\github\tncrazvan\jphp\Status;
 use com\github\tncrazvan\jphp\HttpResponse;
 use com\github\tncrazvan\jphp\JphpInterface;
+/*
+    $argv does not contain the data, it simply contains the length of the header and the length of the body,
+    which are used to read from the stdin correctly.
+*/
 new Jphp($argv,new class() implements JphpInterface{
     public function run(array $_HEADERS, array $_ARGS, array $_QUERY, string $_BODY):HttpResponse{
         
